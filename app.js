3b5f57bddb1e8ae238d79efa9908bedddb6e65b8
dele-rodriguez <114116037+dele-rodriguez@users.noreply.github.com>
2023-04-12 20:01:06 +0100

    Users can now add their secrets

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ mongoose.connect('mongodb://127.0.0.1:27017/userDB')
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -138,12 +139,45 @@ app.route('/secrets')
 .get((req , res) => {
     if (req.isAuthenticated()){
         console.log('User is authenticated!');
-        res.render('secrets');
+        User.find({"secret": {$ne: null}})
+            .then((foundUsers) => {
+                res.render('secrets' , {usersWithSecrets: foundUsers});
+            })
+            .catch((err) => {
+                console.log(err);
+        });
     } else {
         console.log('User is not authenticated!');
         res.redirect('/register');
     }
 
+});
+
+app.route('/submit')
+.get((req , res) => {
+    if (req.isAuthenticated()){
+        console.log('User is authenticated!');
+        res.render('submit');
+    } else {
+        console.log('User is not authenticated!');
+        res.redirect('/register');
+    }
+})
+.post((req , res) => {
+    const submittedSecret = req.body.secret;
+    // console.log(req.user);
+
+    User.findById(req.user.id)
+        .then((foundUser) => {
+            foundUser.secret = submittedSecret;
+            foundUser.save().then(() => {
+                res.redirect('/secrets');
+            });
+
+        })
+        .catch((err) => {
+            console.log(err);
+    });
 });
 
 app.route('/logout')
